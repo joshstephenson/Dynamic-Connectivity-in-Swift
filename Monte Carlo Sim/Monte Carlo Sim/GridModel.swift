0b9944f67cb96c6191b48debc803116d89c00442
Joshua Stephenson <josh@brightmediums.com>
2021-08-21 21:39:44 -0400

    Cleaning up commented code

--- Monte Carlo Sim/Monte Carlo Sim/GridModel.swift
+++ Monte Carlo Sim/Monte Carlo Sim/GridModel.swift
@@ -73,52 +73,6 @@ class GridModel: ObservableObject {
         return sites[i];
     }
     
-//    internal func runTrials() {
-//        var results:[Double] = []
-//
-//        for _ in 1...numTrials {
-//            self.reset()
-//            results.append(self.findPercolationPoint())
-//        }
-//
-//        // find the standard deviation
-//        let expression = NSExpression(forFunction: "stddev:", arguments: [NSExpression(forConstantValue: results)])
-//        let expression2 = NSExpression(forFunction: "average:", arguments: [NSExpression(forConstantValue: results)])
-//
-//        if let standardDeviation = expression.expressionValue(with: nil, context: nil) as? Double, let mean = expression2.expressionValue(with: nil, context: nil) as? Double {
-//
-//            // find the confidence interval based on the pstar
-//            let confidence = confidenceCoefficient * standardDeviation / Double(numTrials).squareRoot()
-//            let confidenceLo = mean - confidence
-//            let confidenceHigh = mean + confidence
-//            let resultString = """
-//
-//                \tTrials: \(numTrials), Grid Size: \(gridSize), Confidence Coefficient: \(confidenceCoefficient)
-//
-//                \tMean: \t\t\t\t\t\t\(String(format: "%.3f", mean))\n\tStandard Deviation: \t\t\(String(format: "%.3f", standardDeviation))\n\t\(Int(confidenceCoefficient * 100))% Confidence interval: \t[\(String(format: "%.3f", confidenceLo)), \(String(format: "%.3f", confidenceHigh))]
-//                """
-//            print(resultString)
-//        }else {
-//            print("Error calculating results")
-//        }
-//    }
-//
-//    private func findPercolationPoint() -> Double {
-//        while(!grid.percolates()){
-//            self.openRandomSite()
-//        }
-//        let percolationPoint = Double(grid.openSitesCount) / Double(gridSize*gridSize)
-//        return percolationPoint
-//    }
-//
-//    private func openRandomSite() {
-//        let row = Int.random(in: 1...self.gridSize)
-//        let col = Int.random(in: 1...self.gridSize)
-//        if let site = self.siteFor(row: row, col: col) {
-//            site.open()
-//        }
-//    }
-    
     public func fillSites() {
         sites.forEach { site in
             site.updateFull()
